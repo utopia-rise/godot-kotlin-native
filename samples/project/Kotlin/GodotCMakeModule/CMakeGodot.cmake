5d5b1ffbc22cff046135f2c63536e856093cdcae
Paul Kholyavko <mrakakuy@yandex.ru>
2018-08-28 13:07:18 +0700

    NativeScript classes generation, output dir.

--- samples/project/Kotlin/GodotCMakeModule/CMakeGodot.cmake
+++ samples/project/Kotlin/GodotCMakeModule/CMakeGodot.cmake
@@ -3,7 +3,7 @@ macro(prepare_godot_args)
             GODOT
             ""
             "NAME;TARGET"
-            "CONFIGS;SOURCES;LIBRARIES;LINKER_OPTS"
+            "CONFIGS;OUTPUT_DIR;LIBRARY_PATH;SOURCES;LIBRARIES;LINKER_OPTS"
             ${ARGN}
     )
 
@@ -11,9 +11,19 @@ macro(prepare_godot_args)
         message(FATAL_ERROR "You must provide a list of configs with registrations for Godot")
     endif ()
 
+    set(GODOT_LIBRARY_PATH_FLAG)
+    if (GODOT_LIBRARY_PATH)
+        set(GODOT_LIBRARY_PATH_FLAG -lib "${GODOT_LIBRARY_PATH}")
+    endif()
+
+    set(GODOT_OUTPUT_DIR_FLAG)
+    if (GODOT_OUTPUT_DIR)
+        set(GODOT_OUTPUT_DIR_FLAG --output-dir "${CMAKE_CURRENT_SOURCE_DIR}/${GODOT_OUTPUT_DIR}")
+    endif()
+
     set(GODOT_CONFIGS_FULL_PATHES "")
     foreach (CONFIG ${GODOT_CONFIGS})
-        list(APPEND GODOT_CONFIGS_FULL_PATHES ${CMAKE_CURRENT_SOURCE_DIR}/${CONFIG})
+        list(APPEND GODOT_CONFIGS_FULL_PATHES "${CMAKE_CURRENT_SOURCE_DIR}/${CONFIG}")
     endforeach ()
 
 endmacro()
@@ -24,19 +34,24 @@ function(godot_library)
 
     if (NOT GDNative)
         klib(NAME GDNative
-            PATH ${GODOT_CMAKE_MODULE_PATH}/lib/GDNative.klib)
+            PATH ${GODOT_CMAKE_MODULE_PATH}/lib/GDNative.klib
+        )
     endif()
 
     if (NOT Godot)
         klib(NAME Godot
             PATH ${GODOT_CMAKE_MODULE_PATH}/lib/Godot.klib
-            LIBRARIES GDNative)
+            LIBRARIES GDNative
+        )
     endif()
 
     add_custom_command(
             OUTPUT ${GODOT_CMAKE_MODULE_PATH}/Entry.kt
             PRE_BUILD
-            COMMAND java -jar ${GODOT_CMAKE_MODULE_PATH}/entry_generator.jar ${GODOT_CONFIGS_FULL_PATHES} -o ${GODOT_CMAKE_MODULE_PATH}/Entry.kt)
+            COMMAND java -jar "${GODOT_CMAKE_MODULE_PATH}/entry_generator.jar" ${GODOT_CONFIGS_FULL_PATHES}
+            -o "${GODOT_CMAKE_MODULE_PATH}/Entry.kt"
+            ${GODOT_LIBRARY_PATH_FLAG} ${GODOT_OUTPUT_DIR_FLAG}
+    )
 
     konanc_dynamic(
             NAME ${GODOT_NAME}
@@ -45,5 +60,19 @@ function(godot_library)
             LIBRARIES GDNative Godot ${GODOT_LIBRARIES}
             LINKER_OPTS ${GODOT_LINKER_OPTS}
     )
+
+    if (GODOT_OUTPUT_DIR)
+        set(OUTPUT_BINARY ${CMAKE_CURRENT_SOURCE_DIR}/${GODOT_OUTPUT_DIR}/${GODOT_NAME}${CMAKE_SHARED_LIBRARY_SUFFIX})
+
+        add_custom_command(
+                OUTPUT ${OUTPUT_BINARY}
+                DEPENDS ${GODOT_NAME}
+                PRE_LINK
+                COMMAND ${CMAKE_COMMAND} -E copy ${LIBRARY_${GODOT_NAME}_OUTPUT} ${OUTPUT_BINARY}
+        )
+        add_custom_target(${GODOT_NAME}.output
+                DEPENDS ${OUTPUT_BINARY}
+                SOURCES ${GODOT_SOURCES})
+    endif()
 
 endfunction()
--- tools/entry_generator/src/main/kotlin/Generator.kt
+++ tools/entry_generator/src/main/kotlin/Generator.kt
@@ -9,17 +9,41 @@ fun main(args: Array<String>) {
 
 
     var output = ""
-    var flag = false
+    var outputFlag = false
+
+    var outputDir = ""
+    var outputDirFlag = false
+
+    var libPath = ""
+    var libPathFlag = false
 
     val configContent = mutableListOf<Classes>()
     for (arg in args) {
-        if (flag) {
+        if (outputFlag) {
             output = arg
-            flag = false
+            outputFlag = false
+            continue
+        }
+        if (outputDirFlag) {
+            outputDir = arg
+            outputDirFlag = false
+            continue
+        }
+        if (libPathFlag) {
+            libPath = arg
+            libPathFlag = false
             continue
         }
         if (arg == "-o") {
-            flag = true
+            outputFlag = true
+            continue
+        }
+        if (arg == "--output-dir") {
+            outputDirFlag = true
+            continue
+        }
+        if (arg == "-lib") {
+            libPathFlag = true
             continue
         }
 
@@ -38,9 +62,40 @@ fun main(args: Array<String>) {
 
     if (output == "")
         error("Output file must be specified with -o key")
+    if (outputDir != "" && libPath == "")
+        error("Path to gdnative-library must be specified with -lib key")
 
 
     val classes = configContent.unite()
 
-    File(output).writeText(classes.generate())
+    val out = File(output)
+    out.parentFile.mkdirs()
+    out.createNewFile()
+
+    out.writeText(classes.generate())
+
+
+    if (outputDir != "") {
+        if (!libPath.startsWith("res://"))
+            libPath = "res://$libPath"
+
+        for (cl in classes.classes) {
+            val out = File("$outputDir/Scripts/${cl.name}.gdns")
+            out.parentFile.mkdirs()
+            out.createNewFile()
+
+            out.writeText("""
+[gd_resource type="NativeScript" load_steps=2 format=2]
+
+[ext_resource path="$libPath" type="GDNativeLibrary" id=1]
+
+[resource]
+
+resource_name = "${cl.name}"
+class_name = "${cl.name}"
+library = ExtResource( 1 )
+            """.trimIndent())
+        }
+    } else
+        println("Warning: output dir not specified, NativeScript classes will not be generated")
 }
