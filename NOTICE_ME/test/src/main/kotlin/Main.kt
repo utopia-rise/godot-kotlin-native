28137c44a3d3ca5d8e82c15898c896ae430928f6
Paul Kholyavko <mrakakuy@yandex.ru>
2018-10-08 23:38:52 +0700

    Test project for coroutines linking.

---
+++ NOTICE_ME/test/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+//import kotlinx.coroutines.*
+
+
+fun main(args: Array<String>) {
+    /*runBlocking {
+        val a = async {
+            println("3")
+        }
+        println("1")
+        delay(2000)
+        println("2")
+        a.await()
+    }*/
+}
