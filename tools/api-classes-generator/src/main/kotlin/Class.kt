7122d16d6fb06da6ebf78cb2c810224ce7bf8b44
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-02-08 10:50:03 +0100

    Format code

--- tools/api-classes-generator/src/main/kotlin/Class.kt
+++ tools/api-classes-generator/src/main/kotlin/Class.kt
@@ -176,7 +176,8 @@ class Class(
                     FunSpec.builder("from")
                             .addModifiers(KModifier.INFIX)
                             .addParameter("other", ClassName(if (node.value.name.isCoreType()) "godot.core" else "godot", node.value.name))
-                            .addStatement("return $name(\"\").apply{ setRawMemory(other.rawMemory) }").build()
+                            .addStatement("return $name(\"\").apply{ setRawMemory(other.rawMemory) }")
+                            .build()
             )
             node = node.parent
         }
