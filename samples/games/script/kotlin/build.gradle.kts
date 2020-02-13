256743be32dd2f75007194d8625c70bcc3def200
Adam Brown <adamwbrown@gmail.com>
2020-02-13 15:07:03 -0700

    game sample now working with new layout

--- samples/games/script/kotlin/build.gradle.kts
+++ samples/games/script/kotlin/build.gradle.kts
@@ -41,7 +41,7 @@ kotlin {
                 }
 
                 libraryPath("samples.gdnlib")
-                generateGDNS("${project.rootDir.absolutePath}/../project")
+                generateGDNS("${project.rootDir.absolutePath}/../..")
 
                 configs(
                         "src/main/kotlin/godot/samples/games/shmup/classes.json",
