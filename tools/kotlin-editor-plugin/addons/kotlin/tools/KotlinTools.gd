17f089262f5d250e61718d35f1e98b161bd747e6
Adam Brown <adamwbrown@gmail.com>
2020-02-20 13:34:45 -0700

    Proof of concept
    - Downloads and extracts kotlin template
    - Can run gradle to config and build

---
+++ tools/kotlin-editor-plugin/addons/kotlin/tools/KotlinTools.gd
@@ -0,0 +1,98 @@
+tool
+extends WindowDialog
+
+const KOTLIN_ZIP := "res://kotlin.zip"
+const DOWNLOAD_FILE := "http://darkrockstudios.com/files/kotlin.zip"
+
+
+func _on_AddSupportButton_pressed():
+	step_1_create_structure()
+
+
+func step_1_create_structure():
+	print("Step 3: Create project structure")
+	var zipFile := File.new()
+	if zipFile.file_exists(KOTLIN_ZIP):
+		print("Template already downloaded")
+		unzip(KOTLIN_ZIP)
+	else:
+		print("Downloading template")
+		download()
+
+
+func download():
+	print("Starting Download...")
+	# Create an HTTP request node and connect its completion signal.
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, "_http_request_completed")
+	
+	# Perform the HTTP request. The URL below returns some JSON as of writing.
+	var error = http_request.request(DOWNLOAD_FILE)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+
+
+func _http_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+	if response_code == 200:
+		print("Download complete.")
+		var zipFile := File.new()
+		zipFile.open(KOTLIN_ZIP, File.WRITE)
+		zipFile.store_buffer(body)
+		zipFile.close()
+		
+		unzip(KOTLIN_ZIP)
+	else:
+		print("Failed to download zip")
+
+
+func unzip(filePath: String):
+	var dir := Directory.new()
+	dir.open("res://")
+	
+	var gdunzip = load('res://addons/gdunzip/gdunzip.gd').new()
+	var loaded = gdunzip.load(filePath)
+	if loaded:
+		for f in gdunzip.files.values():
+			var file = File.new()
+			print(f.file_name)
+			if f.file_name.find("/") > -1:
+				
+				var pathParts = f.file_name.split("/", false)
+				pathParts.remove(pathParts.size()-1) # Remove the file
+				var justDirectories := ""
+				for part in pathParts:
+					justDirectories += "%s/" % part
+					print("Making directories: %s" % justDirectories)
+					dir.make_dir_recursive(justDirectories)
+			
+			file.open(f.file_name, File.WRITE)
+			var uncompressed = gdunzip.uncompress(f.file_name)
+			file.store_buffer(uncompressed)
+			file.close()
+	
+	step_2_cleanup()
+
+
+func step_2_cleanup():
+	print("Step 2: Clean up")
+	var dir = Directory.new()
+	dir.remove(KOTLIN_ZIP)
+	
+	step_3_configure()
+
+
+func step_3_configure():
+	print("Step 3: Configure project")
+	KotlinUtilities.gradle_configure()
+
+
+func _on_ConfigGradleButton_pressed():
+	step_3_configure()
+
+
+func _on_BuildButton_pressed():
+	KotlinUtilities.gradle_build()
+
+
+
