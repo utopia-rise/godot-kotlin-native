ef06e752e35528a900c8363d5ab4ee5d630f0100
Adam Brown <adamwbrown@gmail.com>
2020-02-20 22:44:05 -0700

    Support for Unix systems
    Didn't test it, might have to tweak the exact command sent

--- tools/godot-editor-plugin/addons/kotlin/tools/GradleUtilities.gd
+++ tools/godot-editor-plugin/addons/kotlin/tools/GradleUtilities.gd
@@ -9,14 +9,29 @@ class_name GradleUtilities
 
 
 static func gradle_configure():
-	execute("gradlew.bat")
+	if is_windows():
+		execute("gradlew.bat")
+	else:
+		execute("gradlew")
 
 
 static func gradle_build():
-	execute("build.bat")
+	if is_windows():
+		execute("build.bat")
+	else:
+		execute("build")
 
 
 static func execute(command: String):
 	var output := []
-	OS.execute("CMD.exe", ["/C cd kotlin && %s" % command], true, output)
+	
+	if is_windows():
+		OS.execute("CMD.exe", ["/C cd kotlin && %s" % command], true, output)
+	else:
+		OS.execute("sh", ["cd kotlin && %s" % command], true, output)
+	
 	print(output)
+
+
+static func is_windows() -> bool:
+	return OS.get_name() == "Windows"
--- tools/kotlin-project-template/run_gradle.bat
+++
@@ -1,2 +0,0 @@
-cd kotlin/
-call gradlew.bat %GRADLE_ARG%
