6cdba63ab0edeed3e3a20ca28ad80eb7534662c8
Adam Brown <adamwbrown@gmail.com>
2020-02-22 14:56:07 -0700

    Added selectable build target using -PbuildTarget=release (#35)
    * Added selectable build target using -PbuildTarget=release
    
    Implementation for #28
    Just `target` was a loaded term within the build script, so I opted for `buildTarget` to differentiate it
    
    * Changed buildTarget to buildType
    
    Added to documentation
    
    * Added buildType to GETTING_STARTED
    
    * Added buildType to full build script

--- samples/games/kotlin/build.gradle.kts
+++ samples/games/kotlin/build.gradle.kts
@@ -1,6 +1,7 @@
 val platform: String by project
 val armArch: String by project
 val iosSigningIdentity: String by project
+val buildType: String? by project
 
 buildscript {
     repositories {
@@ -115,7 +116,15 @@ kotlin {
             if (this is org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeCompilation) {
                 println("Configuring target ${this.target.name}")
                 this.target.binaries {
-                    sharedLib(listOf(org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.DEBUG))
+                    val libTarget = when(buildType?.toLowerCase()) {
+                        "release" -> listOf(org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.RELEASE)
+                        "debug" -> listOf(org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.DEBUG)
+                        else -> {
+                            logger.warn("Build target not specified, defaulting to DEBUG. To set release target, specify: -PbuildType=RELEASE")
+                            listOf(org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.DEBUG)
+                        }
+                    }
+                    sharedLib(libTarget)
                 }
                 this.target.compilations.all {
                     dependencies {
