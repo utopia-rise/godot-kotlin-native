43f2f3139db24668507ddddc69cf0f7d2699080f
Adam Brown <adamwbrown@gmail.com>
2020-02-24 13:44:37 -0700

    Added gradle tasks to stage and zip up the kotlin project template
    Maybe we can chain these up to a publish task that will automatically publish them somewhere?
    I was thinking they could live as artifacts in each GitHub release we do.
    The result here is this: <root>/build/kotlin_template.zip

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,3 +21,32 @@ buildscript {
         apply(plugin = "idea")
     }
 }
+
+/**
+ * Packages up the kotlin-project-template which must be published
+ * to GitHub so it can be downloaded by the Godot Editor Plugin.
+ * We want the zip to contain the parent directory 'kotlin/'
+ * So first we copy the template into a directory of that name.
+  */
+tasks.register<Copy>("prepareTemplate") {
+    from("tools/kotlin-project-template/")
+    into("$buildDir/project_template/kotlin")
+    doFirst {
+        mkdir("$buildDir/project_template/kotlin")
+    }
+}
+
+tasks.register<Zip>("packageTemplate") {
+    archiveFileName.set("kotlin_template.zip")
+    destinationDirectory.set(file("$buildDir/"))
+
+    from("$buildDir/project_template")
+
+    dependsOn("prepareTemplate")
+
+    // Clean up the staging directory
+    doLast {
+        delete("$buildDir/project_template")
+    }
+}
+
