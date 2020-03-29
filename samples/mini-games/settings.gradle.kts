78a164aa585da6f222690afb3624d47b88a0eda6
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-03-29 22:34:51 +1100

    .

--- samples/mini-games/settings.gradle.kts
+++ samples/mini-games/settings.gradle.kts
@@ -1,9 +1,11 @@
+// TODO: remove this once we have publishing setup for all artifacts
 includeBuild("../../") {
     dependencySubstitution {
         substitute(module("com.utopia-rise:godot-gradle-plugin")).with(project(":godot-gradle-plugin"))
     }
 }
 
+// TODO: remove this once we have publishing setup for all artifacts
 pluginManagement {
     resolutionStrategy.eachPlugin {
         when (requested.id.id) {
