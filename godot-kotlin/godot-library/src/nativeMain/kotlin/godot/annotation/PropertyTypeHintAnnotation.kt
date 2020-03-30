740bcd41610e062eab5a3b29ea7263678a533a49
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-03-30 16:08:07 +0200

    Rearrange propertyTypeHints and add missing ones from https://github.com/godotengine/godot/blob/3.2/core/object.h (but not deprecated ones!)

--- godot-kotlin/godot-library/src/nativeMain/kotlin/godot/annotation/PropertyTypeHintAnnotation.kt
+++ godot-kotlin/godot-library/src/nativeMain/kotlin/godot/annotation/PropertyTypeHintAnnotation.kt
@@ -10,6 +10,34 @@ We should move that to the idea plugin at some point in time to provide IDE help
  */
 
 /**
+ * Can only be used on Int properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IntRange(val start: Int, val end: Int, val step: Int = -1, val or: Range = Range.NONE)
+
+/**
+ * Can only be used on Float properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class FloatRange(val start: Float, val end: Float, val step: Float = -1f, val or: Range = Range.NONE)
+
+/**
+ * Can only be used on Double properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DoubleRange(val start: Double, val end: Double, val step: Double = -1.0, val or: Range = Range.NONE)
+
+/**
+ * Can only be used on Float and Double properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ExpRange(val start: Float, val end: Float, val step: Float = -1f)
+
+/**
  * This property hint is optional!
  *
  * A Enum that is visible to the editor is always marked as PROPERTY_HINT_ENUM if not defined otherwise
@@ -22,6 +50,20 @@ We should move that to the idea plugin at some point in time to provide IDE help
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class EnumTypeHint(val enumerateAsInt: Boolean = false)
+
+/**
+ * Can only be used on Float and Double properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ExpEasing(val attenuation: Boolean = false, val inout: Boolean = true)
+
+/**
+ * Can only be used on Float and Double properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Lenght(val lenght: Int = -1)
 
 /**
  * Can only be used on Map<Enum, Boolean> properties!
@@ -59,46 +101,25 @@ annotation class Layers2DRender
 annotation class Layers3DRender
 
 /**
- * Can only be used on Int properties!
+ * Can only be used on String and File properties!
  */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ObjectId
+annotation class File(vararg val extensions: String = [], val global: Boolean = false)
 
 /**
- * Can only be used on Int properties!
+ * Can only be used on String properties!
  */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class IntRange(val start: Int, val end: Int, val step: Int = -1, val or: Range = Range.NONE)
+annotation class Dir(val global: Boolean = false)
 
 /**
- * Can only be used on Float properties!
+ * Can only be used on properties that derive from GodotResource!
  */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class FloatRange(val start: Float, val end: Float, val step: Float = -1f, val or: Range = Range.NONE)
-
-/**
- * Can only be used on Double properties!
- */
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class DoubleRange(val start: Double, val end: Double, val step: Double = -1.0, val or: Range = Range.NONE)
-
-/**
- * Can only be used on Float and Double properties!
- */
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class ExpEasing(val attenuation: Boolean = false, val inout: Boolean = true)
-
-/**
- * Can only be used on Float and Double properties!
- */
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class ExpRange(val start: Float, val end: Float, val step: Float = -1f, val or: Range = Range.NONE)
+annotation class ResourceType(vararg val inherits: KClass<out GodotResource> = [GodotResource::class])
 
 /**
  * Can only be used on String properties!
@@ -108,51 +129,53 @@ annotation class ExpRange(val start: Float, val end: Float, val step: Float = -1
 annotation class MultilineText
 
 /**
- * This property hint is ignored! It's here for completion sake and match parity with godot's export hints.
- *
- * A String that is visible to the editor is always marked as PROPERTY_HINT_TYPE_STRING if not defined otherwise
- * through another PropertyTypeHintAnnotation
- *
  * Can only be used on String properties!
  */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class StringTypeHint
+annotation class PlaceHolderText
+
+/**
+ * Can only be used on Color properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ColorNoAlpha
+
+/**
+ * Can only be used on Image properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ImageCompressLossy
+
+/**
+ * Can only be used on Image properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ImageCompressLossLess
+
+/**
+ * Can only be used on Int properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ObjectId
 
 /**
  * Can only be used on String properties!
  */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class Dir
+annotation class TypeString(val baseTypeAsString: String)
 
 /**
- * Can only be used on String and File properties!
+ * Can only be used on NodePath properties!
  */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class File(vararg val extensions: String = [])
-
-/**
- * Can only be used on String and File properties!
- */
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class SaveFile(vararg val extensions: String = [])
-
-/**
- * Can only be used on String and File properties!
- */
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class GlobalDir
-
-/**
- * Can only be used on String and File properties!
- */
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class GlobalFile(vararg val extensions: String = [])
+annotation class NodePathToEditedNode
 
 /**
  * Can only be used on String properties!
@@ -210,26 +233,9 @@ annotation class PropertyOfInstance
 @Retention(AnnotationRetention.RUNTIME)
 annotation class PropertyOfScript
 
-/**
- * Can only be used on String properties!
- */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class PlaceHolderText
-
-/**
- * Can only be used on Color properties!
- */
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class ColorNoAlpha
-
-/**
- * Can only be used on NodePath properties!
- */
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class NodePathToEditedNode
+annotation class ObjectTooBig
 
 /**
  * Can only be used on NodePath properties!
@@ -239,8 +245,15 @@ annotation class NodePathToEditedNode
 annotation class NodePathValidTypes
 
 /**
- * Can only be used on properties that derive from GodotResource!
+ * Can only be used on String and File properties!
  */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ResourceType(vararg val inherits: KClass<out GodotResource> = [GodotResource::class])
+annotation class SaveFile(vararg val extensions: String = [])
+
+/**
+ * Can only be used on String and File properties!
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Max
