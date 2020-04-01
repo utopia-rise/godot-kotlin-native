dd48a1865371dfc969fdde0fd349d309690cb02b
Pierre-Thomas Meisels <dev001@infra.utopia-rise.com>
2020-04-01 18:24:45 +0200

    add sourcesets for godot-library and godot headers as submodule

--- godot-kotlin/godot-library/build.gradle.kts
+++ godot-kotlin/godot-library/build.gradle.kts
@@ -6,13 +6,19 @@ plugins {
 
 //TODO: this needs to be properly configured! This is just a basic setup to be able to implement the annotations
 kotlin {
-    linuxX64()
-    mingwX64()
-    macosX64()
-
-    val nativeMain = sourceSets.create("nativeMain")
+    macosX64("macos")
+    linuxX64("linux")
+    mingwX64("windows")
 
     targets.withType<KotlinNativeTarget> {
-        compilations.getByName("main").defaultSourceSet { dependsOn(nativeMain) }
+        compilations.getByName("main") {
+            defaultSourceSet {
+                kotlin.srcDirs("src/main/nativeGen/kotlin", "src/main/nativeInternal/kotlin", "src/main/nativeCore/kotlin", "src/main/nativePublic/kotlin")
+            }
+            val gdnative by cinterops.creating {
+                defFile("src/main/nativeInterop/cinterop/godot.def")
+                includeDirs("$rootDir/godot-kotlin/godot-headers/", "src/main/nativeInterop/cinterop")
+            }
+        }
     }
 }
