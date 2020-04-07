d419061ecaea62bbf2f7cef9d20a998930c4a9b9
Pierre-Thomas Meisels <dev001@infra.utopia-rise.com>
2020-04-07 14:30:54 +0200

    Add Internal Utils class to have getMethodBind method

---
+++ godot-kotlin/godot-library/src/nativeInternal/kotlin/godot/internal/utils/Utils.kt
@@ -0,0 +1,28 @@
+package godot.internal.utils
+
+import godot.GodotObject
+import godot.gdnative.godot_global_get_singleton
+import godot.gdnative.godot_method_bind
+import godot.gdnative.godot_method_bind_get_method
+import godot.gdnative.godot_variant_as_object
+import kotlinx.cinterop.COpaquePointer
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.cstr
+import godot.core.Variant
+
+
+internal fun getMethodBind(cl: String, m: String): CPointer<godot_method_bind> {
+    return godot_method_bind_get_method(cl, m) ?: throw NotImplementedError("Cannot get method bind for $m in $cl")
+}
+
+
+internal fun getSingleton(cl: String, clOld: String): COpaquePointer {
+    return godot_global_get_singleton(cl.cstr) ?: throw NullPointerException("Cannot get singleton instance for class $clOld")
+}
+
+
+internal fun <T: GodotObject> fromVariant(obj: T, other: Variant): T {
+    obj.setRawMemory(godot_variant_as_object(other.nativeValue)
+            ?: throw NullPointerException("godot_variant_as_object return null for $other"))
+    return obj
+}
