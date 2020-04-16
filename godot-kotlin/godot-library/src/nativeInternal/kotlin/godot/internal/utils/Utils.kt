d9a972927b222ce13d353a82f2bdb0dc6da05044
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-04-16 17:37:16 +0200

    Call pointer extension member instead of getPointer(memscope) in internal Utils methods

--- godot-kotlin/godot-library/src/nativeInternal/kotlin/godot/internal/utils/Utils.kt
+++ godot-kotlin/godot-library/src/nativeInternal/kotlin/godot/internal/utils/Utils.kt
@@ -7,13 +7,13 @@ import kotlinx.cinterop.*
 
 internal fun getMethodBind(cl: String, m: String): CPointer<godot_method_bind> {
     memScoped {
-        return Godot.gdnative.godot_method_bind_get_method!!.invoke(cl.cstr.getPointer(this), m.cstr.getPointer(this)) ?: throw NotImplementedError("Cannot get method bind for $m in $cl")
+        return Godot.gdnative.godot_method_bind_get_method!!.invoke(cl.cstr.getPointer(this), m.cstr.ptr) ?: throw NotImplementedError("Cannot get method bind for $m in $cl")
     }
 }
 
 
 internal fun getSingleton(cl: String, clOld: String): COpaquePointer {
     memScoped {
-        return Godot.gdnative.godot_global_get_singleton!!.invoke(cl.cstr.getPointer(this)) ?: throw NullPointerException("Cannot get singleton instance for class $clOld")
+        return Godot.gdnative.godot_global_get_singleton!!.invoke(cl.cstr.ptr) ?: throw NullPointerException("Cannot get singleton instance for class $clOld")
     }
 }
