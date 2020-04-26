b50c6a1ee476540ab1582c3ac9d520dc5ede9a08
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-04-26 14:16:56 +0200

    Remove godot binaries and add tasks to download them on demand

--- samples/tests/build.gradle.kts
+++ samples/tests/build.gradle.kts
@@ -1,4 +1,6 @@
-import org.apache.tools.ant.taskdefs.condition.Os
+import de.undercouch.gradle.tasks.download.Download
+import org.gradle.internal.os.OperatingSystem
+import org.gradle.nativeplatform.OperatingSystemFamily.*
 
 buildscript {
     repositories {
@@ -20,12 +22,38 @@ plugins {
 godot {
 }
 
+val godotTaskGroup = "godot"
+val binaryOutputDir = "${project.buildDir}/godot/bin"
+
 tasks {
+    val downloadGodotBinaries by creating(Download::class.java) {
+        val filename = getFileName()
+        val url = getUrl(filename)
+        val downloadedBinaryPath = project.file("${project.buildDir}/$filename")
+
+        group = godotTaskGroup
+        description = "Download godot executable for the current operating system."
+        src(url)
+        dest(downloadedBinaryPath)
+        onlyIfNewer(true)
+        onlyIfModified(true)
+    }
+
+    val extractGodotBinaries by creating(Copy::class.java) {
+        group = godotTaskGroup
+        dependsOn(downloadGodotBinaries)
+        from(project.zipTree(project.file("${project.buildDir}/${getFileName()}")))
+        into(binaryOutputDir)
+    }
+
     val executeGodotTests by creating(Exec::class) {
-        when {
-            Os.isFamily(Os.FAMILY_WINDOWS) -> commandLine("${project.rootDir}/bin/Godot_v3.2.1-stable_win64.exe")
-            Os.isFamily(Os.FAMILY_UNIX) -> commandLine("${project.rootDir}/bin/Godot_v3.2.1-stable_x11.64")
-            Os.isFamily(Os.FAMILY_MAC) -> commandLine("${project.rootDir}/bin/Godot.app/Contents/MacOS/Godot")
+        dependsOn(extractGodotBinaries)
+        group = godotTaskGroup
+        when (getCurrentOs()) {
+            WINDOWS, LINUX ->
+                commandLine("$binaryOutputDir/${getFileName().removeSuffix(".zip")}")
+            MACOS ->
+                commandLine("$binaryOutputDir/${getFileName().removeSuffix(".zip")}/Contents/MacOS/Godot")
             else -> throw IllegalStateException("Godot does not support your OS. Cannot execute tests")
         }
     }
@@ -33,4 +61,55 @@ tasks {
     getByName("check") {
         dependsOn(executeGodotTests)
     }
+}
+
+fun getCurrentOs(): String {
+    val current = OperatingSystem.current()
+    return when {
+        current.isLinux -> LINUX
+        current.isMacOsX -> MACOS
+        current.isWindows -> WINDOWS
+        else -> throw AssertionError("Unsupported os: ${current.name}")
+    }
+}
+
+fun getFileName(): String {
+    val currentOs = getCurrentOs()
+    val (base, rc) = getGodotBaseVersionAndPossibleReleaseCandidateVersion()
+
+    val filename = StringBuilder("Godot_v${base}")
+    if (rc == null) {
+        filename.append("-stable")
+    } else {
+        filename.append("-rc${rc}")
+    }
+    when (currentOs) {
+        WINDOWS -> filename.append("_win64.exe.zip")
+        LINUX -> filename.append("_x11.64.zip")
+        MACOS -> filename.append("_osx.64.zip")
+        else -> throw AssertionError("Unsupported platform $currentOs")
+    }
+    return filename.toString()
+}
+
+fun getUrl(filename: String): String {
+    val (base, rc) = getGodotBaseVersionAndPossibleReleaseCandidateVersion()
+    val url = StringBuilder("https://downloads.tuxfamily.org/godotengine")
+    url.append("/${base}")
+    if (rc != null) {
+        url.append("/rc${rc}")
+    }
+    url.append("/$filename")
+    return url.toString()
+}
+
+fun getGodotBaseVersionAndPossibleReleaseCandidateVersion(): Pair<String, String?> {
+    val godotVersion = "3.2.1" //TODO: don't hardcode
+    val match = Regex("(?<base>\\d\\.\\d(?>\\.\\d)?)(?>-rc(?<rc>\\d))?").matchEntire(godotVersion)
+    requireNotNull(match) { "Invalid godot version string: $godotVersion" }
+    // match.groups[0] is always the full match
+    val base = requireNotNull(match.groups[1])
+    val rc = match.groups[2]
+
+    return base.value to rc?.value
 }
