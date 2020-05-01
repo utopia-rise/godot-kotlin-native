7b9d2919748466713635d51afbb9c12cb286bb15
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-05-01 07:38:50 +0200

    Remove custom resources restrictions

--- entry-generation/godot-entry-generator/src/main/kotlin/godot/entrygenerator/generator/provider/ResourceRegistrationValuesHandler.kt
+++ entry-generation/godot-entry-generator/src/main/kotlin/godot/entrygenerator/generator/provider/ResourceRegistrationValuesHandler.kt
@@ -1,7 +1,6 @@
 package godot.entrygenerator.generator.provider
 
 import com.squareup.kotlinpoet.ClassName
-import godot.entrygenerator.extension.isAnyResourceType
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
@@ -14,9 +13,6 @@ class ResourceRegistrationValuesHandler(
     override fun getDefaultValue(): Pair<String, Array<Any>> {
         if (!propertyDescriptor.isLateInit && isVisibleInEditor()) {
             throw IllegalStateException("You initialized the property \"${propertyDescriptor.fqNameSafe}\". Properties of type Resource which are registered using the @RegisterProperty annotation and are visible in the editor are not allowed to have a default value. Use lateinit.")
-        }
-        if (!propertyDescriptor.type.isAnyResourceType()) {
-            throw IllegalStateException("You defined the type of the property \"${propertyDescriptor.fqNameSafe}\" to be a custom resource type. Custom resource types cannot be registered! Use one of the prebuilt ones from Godot!")
         }
         return super.getDefaultValue()
     }
