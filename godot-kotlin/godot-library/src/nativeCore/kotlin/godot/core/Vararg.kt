e112dcb07a9c9813135ff5d45c890c5845597428
Tristan Grespinet <dev003@infra.utopia-rise.com>
2020-07-10 01:17:16 +0200

    added helper function to cast vararg of Variant to Array<Variant>

---
+++ godot-kotlin/godot-library/src/nativeCore/kotlin/godot/core/Vararg.kt
@@ -0,0 +1,151 @@
+package godot.core
+
+inline fun <reified P0> arrayOfVariant(
+    p0: P0
+): Array<Variant> = arrayOf(
+    p0.toVariant()
+)
+
+inline fun <reified P0, reified P1> arrayOfVariant(
+    p0: P0,
+    p1: P1
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant()
+)
+
+inline fun <reified P0, reified P1, reified P2> arrayOfVariant(
+    p0: P0,
+    p1: P1,
+    p2: P2
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant(),
+    p2.toVariant()
+)
+
+inline fun <reified P0, reified P1, reified P2, reified P3> arrayOfVariant(
+    p0: P0,
+    p1: P1,
+    p2: P2,
+    p3: P3
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant(),
+    p2.toVariant(),
+    p3.toVariant()
+)
+
+inline fun <reified P0, reified P1, reified P2, reified P3, reified P4> arrayOfVariant(
+    p0: P0,
+    p1: P1,
+    p2: P2,
+    p3: P3,
+    p4: P4
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant(),
+    p2.toVariant(),
+    p3.toVariant(),
+    p4.toVariant()
+)
+
+inline fun <reified P0, reified P1, reified P2, reified P3, reified P4, reified P5> arrayOfVariant(
+    p0: P0,
+    p1: P1,
+    p2: P2,
+    p3: P3,
+    p4: P4,
+    p5: P5
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant(),
+    p2.toVariant(),
+    p3.toVariant(),
+    p4.toVariant(),
+    p5.toVariant()
+)
+
+inline fun <reified P0, reified P1, reified P2, reified P3, reified P4, reified P5, reified P6> arrayOfVariant(
+    p0: P0,
+    p1: P1,
+    p2: P2,
+    p3: P3,
+    p4: P4,
+    p5: P5,
+    p6: P6
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant(),
+    p2.toVariant(),
+    p3.toVariant(),
+    p4.toVariant(),
+    p5.toVariant(),
+    p6.toVariant()
+)
+
+inline fun <reified P0, reified P1, reified P2, reified P3, reified P4, reified P5, reified P6, reified P7> arrayOfVariant(
+    p0: P0,
+    p1: P1,
+    p2: P2,
+    p3: P3,
+    p4: P4,
+    p5: P5,
+    p6: P6,
+    p7: P7
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant(),
+    p2.toVariant(),
+    p3.toVariant(),
+    p4.toVariant(),
+    p5.toVariant(),
+    p6.toVariant(),
+    p7.toVariant()
+)
+
+inline fun <reified P0, reified P1, reified P2, reified P3, reified P4, reified P5, reified P6, reified P7, reified P8> arrayOfVariant(
+    p0: P0,
+    p1: P1,
+    p2: P2,
+    p3: P3,
+    p4: P4,
+    p5: P5,
+    p6: P6,
+    p7: P7,
+    p8: P8
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant(),
+    p2.toVariant(),
+    p3.toVariant(),
+    p4.toVariant(),
+    p5.toVariant(),
+    p6.toVariant(),
+    p7.toVariant(),
+    p8.toVariant()
+)
+
+inline fun <reified P0, reified P1, reified P2, reified P3, reified P4, reified P5, reified P6, reified P7, reified P8, reified P9> arrayOfVariant(
+    p0: P0,
+    p1: P1,
+    p2: P2,
+    p3: P3,
+    p4: P4,
+    p5: P5,
+    p6: P6,
+    p7: P7,
+    p8: P8,
+    p9: P9
+): Array<Variant> = arrayOf(
+    p0.toVariant(),
+    p1.toVariant(),
+    p2.toVariant(),
+    p3.toVariant(),
+    p4.toVariant(),
+    p5.toVariant(),
+    p6.toVariant(),
+    p7.toVariant(),
+    p8.toVariant(),
+    p9.toVariant()
+)
