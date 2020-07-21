aefe772d40ce8a721e3f2a66693a824453c96541
Tristan Grespinet <dev003@infra.utopia-rise.com>
2020-07-21 22:35:38 +0200

    CMP or CMD: Here the answer

--- godot-kotlin/godot-library/src/nativeInternal/kotlin/godot/internal/type/typeSize.kt
+++ godot-kotlin/godot-library/src/nativeInternal/kotlin/godot/internal/type/typeSize.kt
@@ -36,7 +36,7 @@ internal inline fun Number.toGodotNatural(): Int = this.toInt()
 internal inline fun Int.toGodotNatural(): Int = this
 
 //Because of float precision, it's hard to obtain two perfectly equal real numbers.
-// They are considered equal if the difference is smaller than CMD_EPSILON
+// They are considered equal if the difference is smaller than CMP_EPSILON
 const val CMP_EPSILON: RealT = 0.00001
 fun isEqualApprox(a: RealT, b: RealT, epsilon: RealT = CMP_EPSILON): Boolean {
     //taken from https://github.com/godotengine/godot/blob/d8066aa6a4afb12ffddcec71bd7e051dcd04f3e1/core/math/math_funcs.h#L315
