94b3f8503430ef832f81462cfe305544054f4217
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-07-23 12:08:24 +0200

    [EntryGeneration] Implement support for new godot array types (#162)
    * Implement support for new godot array types
    
    * Revert changes to array creation functions and adapt entry generation to it
    
    * Implement enumVariantArray support
    
    * Fix enum registration
    
    * Add godot files
    
    * Ignore godot import files
    
    * Add testing node
    
    * Use string builder
    
    * Use single return where possible
    
    * Travers supertype list for `godot.core.GodotArray` instead of checking hardcoded strings
    
    * Cleanup code
    
    * Make compatible list check more readable

--- entry-generation/godot-entry-generator/src/main/kotlin/godot/entrygenerator/extension/StringExt.kt
+++ entry-generation/godot-entry-generator/src/main/kotlin/godot/entrygenerator/extension/StringExt.kt
@@ -4,24 +4,50 @@ fun String.isString() = this == "kotlin.String"
 
 fun String.isGodotPrimitive() =
     when (this) {
-        "kotlin.Int" -> true
-        "kotlin.Long" -> true
-        "kotlin.Float" -> true
-        "kotlin.Double" -> true
-        "kotlin.Boolean" -> true
-        "kotlin.Byte" -> true
+        "kotlin.Int",
+        "kotlin.Long",
+        "kotlin.Float",
+        "kotlin.Double",
+        "kotlin.Boolean",
+        "kotlin.Byte",
         "kotlin.Short" -> true
         else -> false
     }
 
 fun String.getAsGodotPrimitive() =
     when (this) {
-        "kotlin.Int" -> "int"
-        "kotlin.Long" -> "int"
-        "kotlin.Float" -> "float"
+        "kotlin.Int",
+        "kotlin.Long",
+        "kotlin.Byte",
+        "kotlin.Short",
+        "kotlin.Enum" -> "int"
+        "kotlin.Float",
         "kotlin.Double" -> "float"
         "kotlin.Boolean" -> "bool"
-        "kotlin.Byte" -> "int"
-        "kotlin.Short" -> "int"
         else -> null
     }
+
+fun String.getCompatibleListType(): String {
+    return when(this) {
+        "godot.core.ObjectArray" -> "Object"
+        "godot.core.EnumArray",
+        "godot.core.IntVariantArray" -> "int"
+        "godot.core.BoolVariantArray" -> "bool"
+        "godot.core.RealVariantArray" -> "float"
+        "godot.core.StringVariantArray" -> "string"
+        "godot.core.AABBArray" -> "AABB"
+        "godot.core.BasisArray" -> "Basis"
+        "godot.core.ColorArray" -> "Color"
+        "godot.core.CoreArray" -> "Core"
+        "godot.core.NodePathArray" -> "NodePath"
+        "godot.core.PlaneArray" -> "Plane"
+        "godot.core.QuatArray" -> "Quat"
+        "godot.core.Rect2Array" -> "Rect2"
+        "godot.core.RIDArray" -> "RID"
+        "godot.core.Transform2DArray" -> "Transform2D"
+        "godot.core.TransformArray" -> "Transform"
+        "godot.core.Vector2Array" -> "Vector2"
+        "godot.core.Vector3Array" -> "Vector3"
+        else -> ""
+    }
+}
--- samples/mini-games/src/godotMain/kotlin/example/TestingClass.kt
+++ samples/mini-games/src/godotMain/kotlin/example/TestingClass.kt
@@ -1,12 +1,8 @@
 package example
 
-import godot.Resource
-import godot.Texture
+import godot.Node
 import godot.annotation.*
-import godot.core.Object
-import godot.core.Variant
 import godot.core.signal
-import godot.core.variantArrayOf
 import godot.registration.RPCMode
 import godot.registration.Range
 
@@ -14,8 +10,8 @@ const val FLAG_1 = 0
 const val FLAG_2 = 1
 internal const val FLAG_3 = 1
 
-@RegisterClass(true)
-class TestingClass : Object() {
+@RegisterClass
+class TestingClass : Node() {
 
     @RegisterSignal
     val signal0 by signal()
@@ -70,73 +66,132 @@ class TestingClass : Object() {
 
     @RegisterProperty
     var flag: Boolean = false
-
+//
+////    @RegisterProperty
+////    val flagImmutable: Boolean = false // <- this should fail as it's immutable
+//
 //    @RegisterProperty
-//    val flagImmutable: Boolean = false // <- this should fail as it's immutable
-
-    @RegisterProperty
-    var variantTest = Variant("test")
-
-    @RegisterProperty
-    lateinit var resourceVisibleInEditor: Texture //should be registered as Texture as Custom resource derives from texture
-
-    @RegisterProperty(false)
-    var resourceNotVisibleInEditor: Resource =
-        Texture() //this should still be registered as Resource and not as Texture
-
-//    @RegisterProperty // <- when visible in editor...
-//    var resourceVisibleInEditorButInitialized: Resource = Resource() // <- ...should fail!
-
-    @RegisterProperty
-    var variantArray = variantArrayOf(variantArrayOf(1, 2))
-
-    @RegisterProperty
-    var variantArrayDifferentTypes = variantArrayOf(variantArrayOf(1, "a")) //should not generate hint string
-
-    @RegisterProperty
-    var variantArrayAny = variantArrayOf(1, 2)
-
-    @RegisterProperty
-    var variantArrayAnyDifferentTypes = variantArrayOf(1, 2, "a", "b") //should not generate hint string
-
-    @RegisterProperty
-    var variantArrayVariant = variantArrayOf(1, 2)
-
-    @RegisterProperty
-    var twoDimensionalArrayVariantArray = variantArrayOf(variantArrayOf(1, 2), variantArrayOf(3, 4))
-
-    @RegisterProperty
-    var threeDimensionalArrayVariantArray = variantArrayOf(
-        variantArrayOf(variantArrayOf(1, 2), variantArrayOf(3, 4)),
-        variantArrayOf(variantArrayOf(5, 6), variantArrayOf(7, 8))
-    )
-
-    @RegisterProperty
-    var enumArray = variantArrayOf(TestEnum.ENUM1, TestEnum.ENUM2)
-
-    @RegisterProperty
-    @EnumFlag
-    var enumFlag = setOf(TestEnum.ENUM1)
-
-
-    companion object {
-        val blubb = 0
-    }
-
-    @RegisterProperty
-    @IntFlag("Flag1", "Flag2", "Flag3", "Flag4", "Flag5")
-    var intFlag = FLAG_1 or FLAG_2 or FLAG_3 or 0 or blubb
-
-    @RegisterProperty
-    @MultilineText
-    var multiLineText = """
-        asdfökjasfdölkjasdflkjaf
-        asdöflkjnasölfkdj
-        asädfkjasödlfkjn
-        asdäfpkjasödflkjm
-    """.trimIndent()
-
-    @RegisterProperty
-    @PlaceHolderText
-    var placeholderText = "somePlaceHolderText"
+//    lateinit var resourceVisibleInEditor: Texture //should be registered as Texture as Custom resource derives from texture
+//
+//    @RegisterProperty(false)
+//    var resourceNotVisibleInEditor: Resource =
+//        Curve() //this should still be registered as Resource and not as Curve
+//
+////    @RegisterProperty // <- when visible in editor...
+////    var resourceVisibleInEditorButInitialized: Resource = Resource() // <- ...should fail!
+//
+//    @RegisterProperty
+//    var pureVariantArray = VariantArray()
+//
+//    @RegisterProperty
+//    var intVariantArray = intVariantArrayOf(1, 2)
+//
+//    @RegisterProperty
+//    var realVariantArray = realVariantArrayOf(1.0, 2.0)
+//
+//    @RegisterProperty
+//    var booleanVariantArray = booleanVariantArrayOf(true, false)
+//
+//    @RegisterProperty
+//    var aabbVariantArray = AABBArray(listOf(AABB(Vector3(), Vector3())))
+//
+//    @RegisterProperty
+//    var aabbVariantArrayOfOther = AABBArray(AABBArray(listOf(AABB(Vector3(), Vector3()))))
+//
+//    @RegisterProperty
+//    var basisVariantArray = BasisArray(listOf(Basis()))
+//
+//    @RegisterProperty
+//    var colorVariantArray = ColorArray(listOf(Color(0, 0, 0)))
+//
+//    @RegisterProperty
+//    var nodePathVariantArray = NodePathArray(listOf(NodePath("some/Node/Path")))
+//
+//    @RegisterProperty
+//    var planeVariantArray = PlaneArray(listOf(Plane()))
+//
+//    @RegisterProperty
+//    var quatVariantArray = QuatArray(listOf(Quat()))
+//
+//    @RegisterProperty
+//    var rect2VariantArray = Rect2Array(listOf(Rect2()))
+//
+//    @RegisterProperty
+//    var ridVariantArray = RIDArray(listOf(RID()))
+//
+//    @RegisterProperty
+//    var transform2DVariantArray = Transform2DArray(listOf(Transform2D()))
+//
+//    @RegisterProperty
+//    var transformVariantArray = TransformArray(listOf(Transform()))
+//
+//    @RegisterProperty
+//    var vector2VariantArray = Vector2Array(listOf(Vector2()))
+//
+//    @RegisterProperty
+//    var vector3VariantArray = Vector3Array(listOf(Vector3()))
+//
+//
+//    @RegisterProperty
+//    var variantArrayDifferentTypes = variantArrayOf(variantArrayOf(1, "a")) //should not generate typed array hint string
+//
+//    @RegisterProperty
+//    var variantArrayAnyDifferentTypes = variantArrayOf(1, 2, "a", "b") //should not generate typed array hint string
+//
+//    @RegisterProperty
+//    var twoDimensionalArrayVariantArray = variantArrayOf(variantArrayOf(1, 2), variantArrayOf(3, 4)) //can not generate typed array hint string
+//
+//    @RegisterProperty
+//    var threeDimensionalArrayVariantArray = variantArrayOf( //can not generate typed array hint string
+//        variantArrayOf(variantArrayOf(1, 2), variantArrayOf(3, 4)),
+//        variantArrayOf(variantArrayOf(5, 6), variantArrayOf(7, 8))
+//    )
+//
+//    @RegisterProperty
+//    var enumArray = EnumArray(listOf(TestEnum.ENUM1, TestEnum.ENUM2)) { enumAsInt ->
+//        TestEnum.values().first { it.ordinal == enumAsInt }
+//    }
+//
+//    @RegisterProperty
+//    var enumArray2 = EnumArray(listOf(TestEnum.ENUM1, TestEnum.ENUM2), { enumAsInt -> TestEnum.values().first { it.ordinal == enumAsInt }}) //not moved out of parenthesis on purpose! To test the behaviour this way as well!
+//
+//
+//    @RegisterProperty
+//    var enumArray3 = EnumArray(EnumArray(listOf(TestEnum.ENUM1, TestEnum.ENUM2)) { enumAsInt ->
+//        TestEnum.values().first { it.ordinal == enumAsInt }
+//    })
+//
+//    @RegisterProperty
+//    var enumArray4 = EnumArray { enumAsInt ->
+//        TestEnum.values().first { it.ordinal == enumAsInt }
+//    }
+//
+//    @RegisterProperty
+//    var enumArray5 = enumVariantArrayOf({ enumAsInt -> TestEnum.values().first { it.ordinal == enumAsInt }}, TestEnum.ENUM1, TestEnum.ENUM2)
+//
+//    @RegisterProperty
+//    @EnumFlag
+//    var enumFlag = setOf(TestEnum.ENUM1)
+//
+//
+//    companion object {
+//        val blubb = 0
+//    }
+//
+//    @RegisterProperty
+//    @IntFlag("Flag1", "Flag2", "Flag3", "Flag4", "Flag5")
+//    var intFlag = FLAG_1 or FLAG_2 or FLAG_3 or 0 or blubb
+//
+//    @RegisterProperty
+//    @MultilineText
+//    var multiLineText = """
+//        asdfökjasfdölkjasdflkjaf
+//        asdöflkjnasölfkdj
+//        asädfkjasödlfkjn
+//        asdäfpkjasödflkjm
+//    """.trimIndent()
+//
+//    @RegisterProperty
+//    @PlaceHolderText
+//    var placeholderText = "somePlaceHolderText"
 }
