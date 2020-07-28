d12ee1099a8114dccd2e6911a369dcaa472fc4d9
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-07-28 14:27:04 +0200

    Remove old binding implementation

--- old/tools/annotations-internal/src/commonMain/kotlin/org/godotengine/kotlin/anntations/internal/RegisterInternal.kt
+++
@@ -1,9 +0,0 @@
-package org.godotengine.kotlin.anntations.internal
-
-@Target(
-        AnnotationTarget.CLASS,
-        AnnotationTarget.FUNCTION,
-        AnnotationTarget.PROPERTY
-)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class RegisterInternal
--- old/tools/entry-generator/src/main/kotlin/org/godotengine/kotlin/entrygenerator/utils/FunctionDescriptorExtensions.kt
+++
@@ -1,8 +0,0 @@
-package org.godotengine.kotlin.entrygenerator.utils
-
-import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
-import org.jetbrains.kotlin.resolve.descriptorUtil.varargParameterPosition
-
-fun CallableMemberDescriptor.hasVarargParameter(): Boolean {
-    return this.varargParameterPosition() != -1
-}
