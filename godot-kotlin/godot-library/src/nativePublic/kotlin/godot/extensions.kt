005f5d257a944d3f585955bb21436e28392cf9f5
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-07-30 18:40:07 +1000

    inline

--- godot-kotlin/godot-library/src/nativePublic/kotlin/godot/extensions.kt
+++ godot-kotlin/godot-library/src/nativePublic/kotlin/godot/extensions.kt
@@ -2,8 +2,8 @@ package godot
 
 import godot.core.NodePath
 
-fun <T: Resource> ResourceLoader.load(path: String, typeHint: String = "", noCache: Boolean = false) = load(
+inline fun <T: Resource> ResourceLoader.load(path: String, typeHint: String = "", noCache: Boolean = false) = load(
     path, typeHint, noCache
 ) as T
 
-fun <T: Node> Node.getNode(path: String) = getNode(NodePath(path)) as T
+inline fun <T: Node> Node.getNode(path: String) = getNode(NodePath(path)) as T
