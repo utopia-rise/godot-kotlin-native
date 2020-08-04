4425896da72a788dff71f1fe9fb0502b5d709160
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-08-05 00:30:33 +1000

    Fix versioning of dev builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,17 @@
-apply {
-    plugin(ReleaseMode::class.java)
+plugins {
+    id("org.ajoberstar.grgit") version "4.0.2"
 }
 
+val currentCommit = grgit.head()
+// check if the current commit is tagged
+var releaseMode = grgit.tag.list().firstOrNull { tag -> tag.commit.id == currentCommit.id } != null
 version = "0.1.0-${DependenciesVersions.godotVersion}"
+if (!releaseMode) {
+    version = "$version-${currentCommit.abbreviatedId}"
+}
 
 val versionString = project.version.toString()
+println("Inferred version: $versionString (release=$releaseMode)")
 
 subprojects {
     group = "com.utopia-rise"
@@ -13,4 +20,5 @@ subprojects {
         mavenLocal()
         jcenter()
     }
+    extra["releaseMode"] = releaseMode
 }
