b6b9e010b02e42d8b6f7d3b2aab87159157bf72b
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-08-08 10:37:10 +0200

    Add missing call function extensions

--- godot-kotlin/godot-library/src/nativePublic/kotlin/godot/extensions.kt
+++ godot-kotlin/godot-library/src/nativePublic/kotlin/godot/extensions.kt
@@ -1,6 +1,7 @@
 package godot
 
 import godot.core.NodePath
+import godot.core.VariantArray
 import godot.core.camelToSnakeCase
 import kotlin.reflect.KFunction
 
@@ -9,4 +10,12 @@ inline fun <T: Node> Node.getNode(path: String) = getNode(NodePath(path)) as T
 
 inline fun <reified T: KFunction<*>> Object.callDeferred(method: T, vararg args: Any?) {
     callDeferred(method.name.camelToSnakeCase(), args)
+}
+
+inline fun <reified T: KFunction<*>> Object.call(method: T, vararg args: Any?) {
+    call(method.name.camelToSnakeCase(), args)
+}
+
+inline fun <reified T: KFunction<*>> Object.callv(method: T, argArray: VariantArray) {
+    callv(method.name.camelToSnakeCase(), argArray)
 }
