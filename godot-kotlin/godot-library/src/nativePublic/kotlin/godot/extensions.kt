4ec95ed716e6bcfdcf25ae4f70703e7bae7de503
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-08-08 10:32:56 +0200

    Add `callDeferred`extension function to `Object`

--- godot-kotlin/godot-library/src/nativePublic/kotlin/godot/extensions.kt
+++ godot-kotlin/godot-library/src/nativePublic/kotlin/godot/extensions.kt
@@ -1,6 +1,12 @@
 package godot
 
 import godot.core.NodePath
+import godot.core.camelToSnakeCase
+import kotlin.reflect.KFunction
 
 @Suppress("NOTHING_TO_INLINE", "UNCHECKED_CAST")
 inline fun <T: Node> Node.getNode(path: String) = getNode(NodePath(path)) as T
+
+inline fun <reified T: KFunction<*>> Object.callDeferred(method: T, vararg args: Any?) {
+    callDeferred(method.name.camelToSnakeCase(), args)
+}
