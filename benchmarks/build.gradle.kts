70bb41902faec7c49ffd82a41a27982ef9dde089
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-08-14 21:30:28 +1000

    .

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -1,4 +1,5 @@
 import godot.gradle.GodotPlatform
+import org.gradle.internal.os.OperatingSystem
 
 plugins {
     kotlin("multiplatform") version "1.3.72"
@@ -19,4 +20,24 @@ godot {
         GodotPlatform.WINDOWS_X64,
         GodotPlatform.OSX_X64
     )
+}
+
+tasks {
+    val build by getting
+    val runBenchmark by creating(Exec::class) {
+        dependsOn(build)
+        val current = OperatingSystem.current()
+        executable = when {
+            current.isLinux -> "bin/linux/godot"
+            current.isMacOsX -> TODO()
+            current.isWindows -> TODO()
+            else -> throw AssertionError("Unsupported os: ${current.name}")
+        }
+        args(mutableListOf(
+            "--no-window",
+            "-q",
+            "-s",
+            "${file("scripts/Main.gd")}"
+        ))
+    }
 }
