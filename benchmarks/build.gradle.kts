fd590afeaab3ae7ccd4b347b686a68f62758030d
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-08-14 21:59:52 +1000

    .

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -1,9 +1,11 @@
 import godot.gradle.GodotPlatform
 import org.gradle.internal.os.OperatingSystem
+import de.undercouch.gradle.tasks.download.Download
 
 plugins {
     kotlin("multiplatform") version "1.3.72"
     id("com.utopia-rise.godot-kotlin") version "0.1.0-3.2-b1e88e7"
+    id("de.undercouch.download") version "4.1.1"
 }
 
 repositories {
@@ -24,11 +26,29 @@ godot {
 
 tasks {
     val build by getting
-    val runBenchmark by creating(Exec::class) {
-        dependsOn(build)
-        val current = OperatingSystem.current()
+    val current = OperatingSystem.current()
+
+    val downloadGodot by creating(Download::class) {
+        src(when {
+            current.isLinux -> "https://downloads.tuxfamily.org/godotengine/3.2.2/Godot_v3.2.2-stable_linux_headless.64.zip"
+            current.isMacOsX -> TODO()
+            current.isWindows -> TODO()
+            else -> throw AssertionError("Unsupported os: ${current.name}")
+        })
+        dest("$buildDir/godot.zip")
+        onlyIfNewer(true)
+    }
+
+    val unzipGodot by creating(Copy::class) {
+        dependsOn(downloadGodot)
+        from(zipTree(downloadGodot.dest))
+        into("$buildDir/extracted")
+    }
+
+    val runBenchmarks by creating(Exec::class) {
+        dependsOn(build, unzipGodot)
         executable = when {
-            current.isLinux -> "bin/linux/godot"
+            current.isLinux -> "$buildDir/extracted/Godot_v3.2.2-stable_linux_headless.64"
             current.isMacOsX -> TODO()
             current.isWindows -> TODO()
             else -> throw AssertionError("Unsupported os: ${current.name}")
